ca3a5f0d79c7318cb299b1eb8783122c5a89e21a
msun0215 <msun0215@skuniv.ac.kr>
2023-09-26 11:02:35 +0900

    0926_am

--- front/src/App.js
+++ front/src/App.js
@@ -23,50 +23,8 @@ function App() {
     const toggleMenu = () => {
       setMenu(isOpen => !isOpen);   // boolean으로 on/off 
     }
-<<<<<<< HEAD
 
   }
-=======
-  }
-  const timeSlots = [
-    '09-10',
-    '10-11',
-    '11-12',
-    '12-13',
-    '13-14',
-    '14-15',
-    '15-16',
-    '16-17',
-    '17-18'
-  ];
-
-  // 초기 시간표 데이터 (빈 과목 배열)
-  const initialTimetable = timeSlots.map((slot) => ({
-    time: slot,
-    subject: '',
-  }));
-
-  // 시간표 데이터를 state로 관리
-  const [timetable, setTimetable] = useState(initialTimetable);
-
-  // 과목 색상 매핑
-  const subjectColors = {
-    '수학': 'red',
-    '과학': 'blue',
-    '영어': 'green',
-    // 추가 과목 및 색상을 여기에 추가할 수 있습니다.
-  };
-
-  // 과목을 선택하여 해당 시간대에 색상을 지정
-  const handleSubjectSelection = (index) => {
-    const selectedSubject = prompt('과목을 입력하세요:');
-    if (selectedSubject) {
-      const updatedTimetable = [...timetable];
-      updatedTimetable[index].subject = selectedSubject;
-      setTimetable(updatedTimetable);
-    }
-  };
->>>>>>> e92641f7bf48f007ef4b0eacbc6adc7e742036a4
 
   return (
     <div class="full">
@@ -76,12 +34,6 @@ function App() {
         <body>
           <div class="container">
             <div id='column' class='symbol'>배너
-<<<<<<< HEAD
-<<<<<<< HEAD
-            </div>
-=======
-=======
->>>>>>> e92641f7bf48f007ef4b0eacbc6adc7e742036a4
               <nav>
                 <ul className="header-wrapper">
                   <li><a href="https://www.skuniv.ac.kr/main">학교 홈페이지 바로가기</a></li>
@@ -90,10 +42,6 @@ function App() {
                   <li><a href="#"><input type="text" name="search" placeholder="검색어를 입력하세요" autoComplete='off' /></a></li>
                 </ul>
               </nav></div>
-<<<<<<< HEAD
->>>>>>> a7c126fb702e7c15b6230cd6ae1685d0e1d13781
-=======
->>>>>>> e92641f7bf48f007ef4b0eacbc6adc7e742036a4
             <div id='column'>
               <div id='column2'>로그인<br />
                 <input type="text" id='id' value='ID 입력' />
@@ -102,74 +50,32 @@ function App() {
               </div>
               <div id='column2'>공지사항</div>
             </div>
-<<<<<<< HEAD
             <div id='column'>시간표</div>
-<<<<<<< HEAD
-            <div>
-            <nav>
-                <ul className="header-wrapper">
-                  <li><a href="https://www.skuniv.ac.kr/main">학교 홈페이지 바로가기</a></li>
-                  <li><a href="https://www.skuniv.ac.kr/notice">학교 공지사항 바로가기</a></li>
-                  <li><a href="https://www.skuniv.ac.kr/academic_calendar">학사일정 바로가기</a></li>
-                  <li><a href="#"><input type="text" name="search" placeholder="검색어를 입력하세요" autoComplete='off' /></a></li>
-                </ul>
-              </nav>
-            </div>
           </div>
           <Sidebar width={320}></Sidebar>
-          <div class="refe">
-            <a href="https://www.skuniv.ac.kr/main"><span>학교 홈페이지</span></a>
-            <a href="https://www.skuniv.ac.kr/notice"><span>학교 공지사항</span></a>
-            <a href="https://www.skuniv.ac.kr/academic_calendar"><span>학사일정</span></a>
-            <a href="https://www.skuniv.ac.kr/phone"><span>교내 연락처</span></a>
-          </div>
         </body>
-        <footer>
-          명Kay검진 &copy; <span>{thisYear()}</span>
+        <div class="underlink">
+          <span class="contact">
+            <a href="https://www.skuniv.ac.kr/main">학교 홈페이지 바로가기</a>
+          </span>
 
-=======
-          </div>
-          <Sidebar width={320}></Sidebar>
-        </body>
-        <div>
-=======
-            <div id='column'>
-            <h1>오늘의 시간표</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>시간</th>
-            <th>과목</th>
-          </tr>
-        </thead>
-        <tbody>
-          {timetable.map((item, index) => (
-            <tr key={index}>
-              <td>{item.time}</td>
-              <td
-                style={{ backgroundColor: subjectColors[item.subject] || 'white' }}
-                onClick={() => handleSubjectSelection(index)}
-              >
-                {item.subject}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-            </div>
-          </div>
-          <Sidebar width={320}></Sidebar>
-        </body>
-        <div class='email'>
->>>>>>> e92641f7bf48f007ef4b0eacbc6adc7e742036a4
-            문의 이메일 : gksrbgks713@skuniv.ac.kr
+          <span class="contact">
+            <a href="https://www.skuniv.ac.kr/notice">학교 공지사항 바로가기</a>
+          </span>
+
+          <span class="contact">
+            <a href="https://www.skuniv.ac.kr/academic_calendar">학사일정 바로가기</a>
+          </span>
+
+          <span class="contact">
+            <a href="https://www.skuniv.ac.kr/contact">학교 연락처</a>
+          </span>
+        </div>
+        <div class="email">
+          문의 이메일 : gksrbgks713@skuniv.ac.kr
         </div>
         <footer>
           명Kay검진 &copy; <span>{thisYear()}</span>
-<<<<<<< HEAD
->>>>>>> a7c126fb702e7c15b6230cd6ae1685d0e1d13781
-=======
->>>>>>> e92641f7bf48f007ef4b0eacbc6adc7e742036a4
         </footer>
 
       </div>
